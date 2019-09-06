dc7c3a12306ac88826527421356e35e097500edb
Octavio Renteria <renteriao@hfi-inc.com>
2019-09-06 09:41:17 -0700

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+//guardar cache dynamico
+function actualizaCacheDinamico( dynamicCache,req,res) {
+if(res.ok){
+ return caches.open(dynamicCache).then( cache=> {
+    cache.put(req, res.clone());
+        return res.clone();
+ });
+
+} else{
+    return res;
+
+}
+
+}
