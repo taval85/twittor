5db444035e86ed7523524c99045b0201ead5d399
Octavio Renteria <renteriao@hfi-inc.com>
2019-09-11 11:21:12 -0700

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmututable-v1';
 
 
@@ -46,6 +46,10 @@ self.addEventListener('activate', e => {
     const activacion =caches.keys().then(keys =>{
         keys.forEach(key=> {
             if( key !== STATIC_CACHE && key.includes('static')){
+                return  caches.delete(key);
+            }
+
+            if( key !== DYNAMIC_CACHE && key.includes('dynamic')){
                 return  caches.delete(key);
             }
         });
